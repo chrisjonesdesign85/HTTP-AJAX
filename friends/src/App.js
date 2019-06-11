9c99bc5a3f81f705881be83514cc028acd7beb2c
Chris Jones <chrisjonesdesign85@gmail.com>
2019-06-11 04:20:24 -0400

    setting up App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,12 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+import axios from 'axios';
+import Friends from '.components/FriendsList';
+import { BrowserRouter as Route } from 'react-router-dom';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: [],
+      newFriend: []
+    }
+  }
+
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.ListeningStateChangedEvent({ friends: res.data }))
+      .catch(err => console.log(err))
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" render={(props) => (
+          <Friends {...props} friends={this.state.friends} />
+        )} />
+
+
+      </div>
+    );
+  }
 }
+
 
 export default App;
